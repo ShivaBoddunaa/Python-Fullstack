c1e3b7b325e02dbe08a501a6f14f4ad1d35b5dc1
shiva bodduna <shivabodduna123@gmail.com>
2025-10-18 12:08:55 +0530

    this folder belongs to sets assignments

---
+++ Set/Assign1.py
@@ -0,0 +1,6 @@
+my_game={"cricket","football","tenis"}
+friends_games={"badminton","cricket","chess"}
+# combine=my_game+friends_games   #wrong approch
+union=my_game|friends_games
+# union=my_game.union(friends_games)
+print(union)
---
+++ Set/Assign4.py
@@ -0,0 +1,6 @@
+my_games = {"cricket", "football", "tennis"}
+my_games.clear()
+print(my_games)
+my_games.update({"baseball","golf","tennis"})
+games=my_games.copy()
+print(games)
